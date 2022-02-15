301312f4cf2cad9221371433df13decb1c65cdd6
eduwer <eloitrocy@hotmail.fr>
2022-02-15 01:40:00 +0100

    Open / Close of the overlay on the right conditions: OK

--- main.c
+++ main.c
@@ -2,85 +2,181 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 #include <X11/Xatom.h>
-#include <X11/extensions/Xcomposite.h>
+#include <X11/extensions/shape.h>
+#include <X11/extensions/Xfixes.h>
+#include <X11/extensions/XInput2.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 Display	*dis;
 GC		gc;
 Window	win;
+Window	root;
 int		screen;
+int		xiOpcode;
+const double	alpha = 0.2;
+const char		*base_color = "#ffffff";
 
-Display *open_display() {
-	unsigned long	black, white;
+// call AFTER the window is shown with XMapRaised
+void setAbove() {
+	XEvent event;
+	Atom wm_state = XInternAtom(dis, "_NET_WM_STATE", False);
+	Atom wm_state_above = XInternAtom(dis, "_NET_WM_STATE_ABOVE", False);
+	XChangeProperty(dis, win, wm_state, XA_ATOM, 32, PropModeReplace, (unsigned char *)&wm_state_above, 1);
+}
 
-	dis = XOpenDisplay(NULL);
-	screen = DefaultScreen(dis);
-	black = BlackPixel(dis, screen);
+void removeWindowInterface() {
+	Atom type = XInternAtom(dis,"_NET_WM_WINDOW_TYPE", False);
+    Atom value = XInternAtom(dis,"_NET_WM_WINDOW_TYPE_SPLASH", False);
+	XChangeProperty(dis, win, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
+}
+
+void setTransparent() {
+	unsigned long opacity = (unsigned long)(0xFFFFFFFFul * alpha);
+	Atom XA_NET_WM_WINDOW_OPACITY = XInternAtom(dis, "_NET_WM_WINDOW_OPACITY", False);
+	XChangeProperty(dis, win, XA_NET_WM_WINDOW_OPACITY, XA_CARDINAL, 32, PropModeReplace, (unsigned char *)&opacity, 1L);
+}
+
+void setDontInterceptInputs() {
+	XRectangle rect;
+	XserverRegion region = XFixesCreateRegion(dis, &rect, 1);
+	XFixesSetWindowShapeRegion(dis, win, ShapeInput, 0, 0, region);
+	XFixesDestroyRegion(dis, region);
+}
+
+unsigned long getColor(const char *colorString) {
+	XColor color;
+	Colormap colormap;
+
+	colormap = DefaultColormap(dis, 0);
+	XParseColor(dis, colormap, colorString, &color);
+	XAllocColor(dis, colormap, &color);
+	return color.pixel;
+}
+
+Display *open_overlay() {
+	unsigned long	white;
+
 	white = WhitePixel(dis, screen);
 
 	XVisualInfo vinfo;
 	XMatchVisualInfo(dis, screen, 32, TrueColor, &vinfo);
-
-	win = XCompositeGetOverlayWindow(dis, DefaultRootWindow(dis));
-
-	// win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0, 500, 400, 5, white, white);
-
-	/*double alpha = 0.2;
-	unsigned long opacity = (unsigned long)(0xFFFFFFFFul * alpha);
-	Atom XA_NET_WM_WINDOW_OPACITY = XInternAtom(dis, "_NET_WM_WINDOW_OPACITY", False);
-	XChangeProperty(dis, win, XA_NET_WM_WINDOW_OPACITY, XA_CARDINAL, 32, PropModeReplace, (unsigned char *)&opacity, 1L);*/
-
-	/*Atom type = XInternAtom(dis,"_NET_WM_WINDOW_TYPE", False);
-    Atom value = XInternAtom(dis,"_NET_WM_WINDOW_TYPE_SPLASH", False);
-	XChangeProperty(dis, win, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);*/
-
-	XSetStandardProperties(dis, win, "window", "lol", None, NULL, 0, NULL);
-	XSelectInput(dis, win, ExposureMask | ButtonPressMask | KeyPressMask);
+	win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0, 42, 42, 42, white, getColor(base_color));
 	gc = XCreateGC(dis, win, 0, 0);
 
+	setTransparent();
+	removeWindowInterface();
+	setDontInterceptInputs();
+
 	XClearWindow(dis, win);
-	XMapRaised(dis, win);
+	XMapWindow(dis, win);
+	setAbove();
+
+	XMoveResizeWindow(dis, win, 0, 0, DisplayWidth(dis, screen), DisplayHeight(dis, screen));
 
 	return (dis);
 }
 
-void close_x() {
+void close_overlay() {
 	XFreeGC(dis, gc);
 	XDestroyWindow(dis, win);
-	XCloseDisplay(dis);
 }
 
-void redraw() {
-	unsigned long white = WhitePixel(dis, screen);
-	XSetBackground(dis, gc, BlackPixel(dis, screen));
-	XSetForeground(dis, gc, white);
-	XFillRectangle(dis, win, gc, 0, 0, 300, 200);
+void register_keys() {
+	XIEventMask m;
+	m.deviceid = XIAllMasterDevices;
+	m.mask_len = XIMaskLen(XI_LASTEVENT);
+	m.mask = calloc(m.mask_len, sizeof(char));
+	XISetMask(m.mask, XI_RawKeyPress);
+	XISetMask(m.mask, XI_RawKeyRelease);
+	XISetMask(m.mask, XI_RawButtonPress);
+	XISetMask(m.mask, XI_RawButtonRelease);
+	XISelectEvents(dis, root, &m, 1);
+	XSync(dis, false);
+	free(m.mask);
+}
+
+void register_window_move() {
+	XSelectInput(dis, root, SubstructureNotifyMask);
+}
+
+void loop() {
+	XEvent	ev;
+	XGenericEventCookie *cookie = (XGenericEventCookie*)&ev.xcookie;
+	bool	ctrl_down = false;
+	bool	configuring = false;
+	bool	opened = false;
+
+	KeyCode	ctrll = XKeysymToKeycode(dis, XK_Control_L);
+	KeyCode ctrlr = XKeysymToKeycode(dis, XK_Control_R);
+
+	while (True) {
+		XNextEvent(dis, &ev);
+		if (XGetEventData(dis, cookie)) {
+			if (cookie->type == GenericEvent && cookie->extension == xiOpcode) {
+                if (cookie->evtype == XI_RawKeyPress
+					&& (((XIRawEvent *)cookie->data)->detail == ctrll
+						|| ((XIRawEvent *)cookie->data)->detail == ctrlr)) {
+							ctrl_down = true;
+							if (!opened && configuring && ctrl_down) {
+								opened = true;
+								printf("Open\n");
+								open_overlay();
+							}
+				}
+				if (cookie->evtype == XI_RawKeyRelease
+					&& (((XIRawEvent *)cookie->data)->detail == ctrll
+						|| ((XIRawEvent *)cookie->data)->detail == ctrlr)) {
+							ctrl_down = false;
+							if (opened) {
+								opened = false;
+								printf("Cancel\n");
+								close_overlay();
+							}
+				}
+				if (cookie->evtype == XI_RawButtonPress
+					&& (((XIRawEvent *)cookie->data)->detail == Button1
+						|| ((XIRawEvent *)cookie->data)->detail == Button3)) {
+					// No op ?
+				}
+				if (cookie->evtype == XI_RawButtonRelease
+					&& (((XIRawEvent *)cookie->data)->detail == Button1
+						|| ((XIRawEvent *)cookie->data)->detail == Button3)) {
+							configuring = false;
+							if (opened) {
+								opened = false;
+								printf("Snap\n");
+								close_overlay();
+							}
+				}
+			}
+		}
+		if (ev.type == ConfigureNotify) {
+			configuring = true;
+		}
+		XFreeEventData(dis, cookie);
+	}
+}
+
+void getDefaults() {
+	dis = XOpenDisplay(NULL);
+	screen = XDefaultScreen(dis);
+	root = DefaultRootWindow(dis);
+
+	// Test for XInput 2 extension
+	int queryEvent, queryError;
+	if (!XQueryExtension(dis, "XInputExtension", &xiOpcode,
+				&queryEvent, &queryError)) {
+		fprintf(stderr, "X Input extension not available\n");
+		exit(2);
+	}
 }
 
 int main(int argc, char **argv) {
-
-	Display	*dis;
-	XEvent	event;
-	KeySym	key;
-	char	text[255];
-
-
-	dis = open_display();
-	while (1) {
-		XNextEvent(dis, &event);
-
-		if (event.type == Expose && event.xexpose.count == 0) {
-			redraw();
-		} else if (event.type == KeyPress && XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
-			if (text[0] == 'q' || text[0] == 27) {
-				close_x();
-				return (0);
-			}
-		}
-		if (event.type == ButtonPress) {
-			printf("Click %d %d\n", event.xbutton.x, event.xbutton.y);
-		}
-	}
-
+	getDefaults();
+	register_keys();
+	register_window_move();
+	loop();
 	return (0);
 }
